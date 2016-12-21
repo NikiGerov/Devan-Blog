9e84e3e846fb5cb5a3cfffe3d298a4b4a9f74e9d
NikiGerov <nikolay.gerov@gmail.com>
2016-12-21 19:28:21 +0200

    Added new bootstrap and some other adjustments

--- softuniblog/Blog/App_Start/BundleConfig.cs
+++ softuniblog/Blog/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Blog
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrapUnited.css",
                       "~/Content/site.css"));
         }
     }
